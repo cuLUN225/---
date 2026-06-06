491ebb8d7f6aa90d79618a0c0e3fedec9630c50e
cuLUN225 <mizuicy893@gmail.com>
2026-06-06 20:59:17 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "made by Zettaaaaa",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#FFFFFF", 
+      secondary: "#941E3B", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
